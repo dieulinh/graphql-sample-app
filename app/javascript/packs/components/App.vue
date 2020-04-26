2c5eda6269e8142f250dba9856dceb6f9592c1b1
Linh Nguyen <linhvn09@gmail.com>
2020-04-26 11:11:41 +0700

    restruct app

--- app/javascript/packs/components/App.vue
+++ app/javascript/packs/components/App.vue
@@ -90,57 +90,15 @@
       </div>
     </div>
     <div class="main">
-      <div class="sidebar">
-        <ul class="sc-dREXXX hOtZfH mdc-list">
-          <a href="/" class="sc-kcbnda kXpcCv">
-            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
-              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Home</p>
-            </li>
-          </a>
-          <a href="/competitions" class="sc-kcbnda kXpcCv">
-            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
-              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Compete</p>
-            </li>
-          </a>
-          <a href="/datasets" class="sc-kcbnda kXpcCv">
-            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
-              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Data</p>
-            </li>
-          </a>
-          <a href="/notebooks" class="sc-kcbnda kXpcCv">
-            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
-              
-              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cEAhaW">Notebooks</p>
-            </li>
-          </a>
-          <a href="/discussion" class="sc-kcbnda kXpcCv">
-            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
-              
-              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Discuss</p>
-            </li>
-          </a>
-          <a href="/learn" class="sc-kcbnda kXpcCv">
-            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
-              
-              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Courses</p>
-            </li>
-          </a>
-          <a class="sc-kcbnda kXpcCv">
-            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
-              
-              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">More</p>
-            </li>
-          </a>
-          <div leftoffset="24px" class="sc-cHSUfg hakoge mdc-menu-surface--anchor"></div>
-        </ul>
-      </div>
-      <div class="page-content">Page content</div>
+      <lesson-component />
     </div>
   </div>
 </template>
 
 <script>
+import LessonComponent from './Lesson'
 export default {
+  components: {LessonComponent},
   data: function() {
     return {
       message: "Hello Vue!"
---
+++ app/javascript/packs/components/Lesson.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="sidebar">
+        <ul class="sc-dREXXX hOtZfH mdc-list">
+          <a href="/" class="sc-kcbnda kXpcCv">
+            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
+              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Home</p>
+            </li>
+          </a>
+          <a href="/competitions" class="sc-kcbnda kXpcCv">
+            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
+              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Compete</p>
+            </li>
+          </a>
+          <a href="/datasets" class="sc-kcbnda kXpcCv">
+            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
+              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Data</p>
+            </li>
+          </a>
+          <a href="/notebooks" class="sc-kcbnda kXpcCv">
+            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
+              
+              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cEAhaW">Notebooks</p>
+            </li>
+          </a>
+          <a href="/discussion" class="sc-kcbnda kXpcCv">
+            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
+              
+              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Discuss</p>
+            </li>
+          </a>
+          <a href="/learn" class="sc-kcbnda kXpcCv">
+            <li class="mdc-list-item sc-hcmgZB kXNAiR" role="listitem" tabindex="-1">
+              
+              <p class="sc-kEYyzF sc-kkGfuU sc-dHmInP cyYMOc">Courses</p>
+            </li>
+          </a>
+          <a class="sc-kcbnda kXpcCv">
+            <li class="mdc-list-item" role="listitem" tabindex="-1">
+              
+              More
+            </li>
+          </a>
+          <div leftoffset="24px" class="sc-cHSUfg hakoge mdc-menu-surface--anchor"></div>
+        </ul>
+      </div>
+      <div class="page-content">Page content</div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
