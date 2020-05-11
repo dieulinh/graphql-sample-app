c3e6944b708638de2483ccaa824490c3fcee2d36
Linh Nguyen <linhvn09@gmail.com>
2020-05-11 13:45:32 +0700

    add sidekiq routes

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-  domain: 'heroku.com',
-  address:        "smtp.sendgrid.net",
-  port:           "587",
-  authentication: :plain,
-  user_name:      ENV['SENDGRID_USERNAME'],
+# ActionMailer::Base.smtp_settings = {
+#   domain: 'heroku.com',
+#   address:        "smtp.sendgrid.net",
+#   port:           "587",
+#   authentication: :plain,
+#   user_name:      ENV['SENDGRID_USERNAME'],
 
-  password:       ENV['SENDGRID_PASSWORD']
-}
+#   password:       ENV['SENDGRID_PASSWORD']
+# }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
+Sidekiq::Web.set :sessions,       Rails.application.config.session_options
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
