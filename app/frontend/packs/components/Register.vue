e0bdc78a9f4d1345b9a0503d647cdaa6b0662120
Linh Nguyen <linhvn09@gmail.com>
2020-05-29 12:34:29 +0700

    update required mark for registration form

--- app/frontend/packs/components/Register.vue
+++ app/frontend/packs/components/Register.vue
@@ -5,9 +5,9 @@
       <div class="pd-30 wht-bg round-corner">
        <div class="panel register-wrapper wht-bg">
           <label class="form-title">Register new account</label>
-          <input type="text" v-model="email" placeholder="Email" :class="{'field-error': invalidEmail}"/>
-          <input type="password" v-model="password" placeholder="Password" :class="{'field-error': invalidPassword}"/>
-          <input type="password" v-model="password_confirmation" placeholder="Re-enter password" :class="{'field-error': invalidConfirmPassword}"/>
+          <input type="text" v-model="email" placeholder="Email (*)" :class="{'field-error': invalidEmail}"/>
+          <input type="password" v-model="password" placeholder="Password (*)" :class="{'field-error': invalidPassword}"/>
+          <input type="password" v-model="password_confirmation" placeholder="Re-enter password (*)" :class="{'field-error': invalidConfirmPassword}"/>
           <button @click="registerUser()" class="btn btn-primary">Register</button>
         </div>
       </div>
@@ -54,7 +54,10 @@ export default {
     },
 
     registerUser() {
-      if (!this.validateUser()) {return}
+      if (!this.validateUser()) {
+        this.$store.dispatch('setFlashMessage', {text: 'Email or password invalid', type: 'error'});
+        return
+      }
       this.$store.dispatch('register', {email: this.email, password: this.password, password_confirmation: this.password_confirmation})
       .then((rs) => {
         if(!this.errors) {
