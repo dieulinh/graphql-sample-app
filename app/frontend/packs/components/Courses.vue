04283d04589aada8e7a225f9c96003284436e868
Linh Nguyen <linhvn09@gmail.com>
2020-06-29 11:17:46 +0700

    change style course title

--- app/frontend/packs/components/Courses.vue
+++ app/frontend/packs/components/Courses.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="course-wrapper">
-    
     <div v-for="course in courses" :key="course.node&&course.node.id" class="course-card">
       <div v-if="course.node">
         <router-link class="course-link"
                     :to='{name: "CoursePage", params: {courseId: course["node"]&&course["node"].id}}'
                   >
 
-          <h3>{{course["node"]&&course["node"].courseName}} </h3>
+          <h3 class="course-title">{{course["node"]&&course["node"].courseName}} </h3>
           <div class="course-desc" v-html="course.node&&course.node.description">
           </div>
           <img :src="course.node&&course.node.courseCoverUrl" class="course-cover"/>
@@ -28,7 +27,7 @@ export default {
   },
   created() {
 
-    axios.get(coursesApiUrl 
+    axios.get(coursesApiUrl
       )
       .then((result) => {
         this.courses = result.data.data.coursesConnection.edges
