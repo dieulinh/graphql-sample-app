0b20d85b0e08b3383f8f29a1faa26a1ebdfe69c3
Linh Nguyen <linhvn09@gmail.com>
2022-03-19 11:49:00 +0700

    remove config base url

--- app/frontend/packs/services/Server.js
+++ app/frontend/packs/services/Server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import User from './User';
 const server = axios.create({
-  baseURL: process.env.API_SERVER_URL
+
 })
 server.interceptors.request.use((config) => {
   const token = User.getUserToken();
