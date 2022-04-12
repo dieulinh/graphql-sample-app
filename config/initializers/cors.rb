ec3feb5099088f65bccf4022ed7428eb2aab53f7
Linh Nguyen <linhvn09@gmail.com>
2022-04-12 16:03:59 +0700

    add more origins and update schema

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'iclass-fe.herokuapp.com', 'localhost:3000'
+    origins 'iclass-fe.herokuapp.com', 'localhost:3000', 'www.bikiin.com'
     resource '/public/*', headers: :any, methods: :get
 
     # Only allow a request for a specific host
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_035712) do
+ActiveRecord::Schema.define(version: 2022_04_11_115735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2022_03_28_035712) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ip_address"
   end
 
   create_table "posts", force: :cascade do |t|
