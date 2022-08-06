ef5752e7f7b74ac19cbd66537a0f5d62ffaa6ba0
Linh Nguyen <linhvn09@gmail.com>
2022-08-06 18:47:58 +0700

    restyle

--- app/frontend/packs/components/App.vue
+++ app/frontend/packs/components/App.vue
@@ -18,23 +18,20 @@
       </div>
 
       <flash-message :message="fMessage"></flash-message>
-      <div class="menu-right mg-5">
-        <div class="btn-group-menu">
-          <template v-if="!authenticated">
-            <a href="javascript:void(0)" @click="register()" class="btn btn-secondary btn-small">
-          Register</a>
-            <a href="javascript:void(0)" @click="login()" class="btn btn-primary btn-small">
-          Login</a>
-          </template>
+      <div class="btn-group-menu">
+        <template v-if="!authenticated">
+          <a href="javascript:void(0)" @click="register()" class="btn btn-secondary btn-small">
+        Register</a>
+          <a href="javascript:void(0)" @click="login()" class="btn btn-primary btn-small">
+        Login</a>
+        </template>
 
-          <template v-if="authenticated">
-            <a href="javascript:void(0)" @click="addCourse()" class="btn btn-primary btn-small">
-            +Course</a>
-            <a href="javascript:void(0)" @click="logout()" class="btn btn-secondary btn-small">
-          Logout</a>
-          </template>
-
-        </div>
+        <template v-if="authenticated">
+          <a href="javascript:void(0)" @click="addCourse()" class="btn btn-primary btn-small">
+          +Course</a>
+          <a href="javascript:void(0)" @click="logout()" class="btn btn-secondary btn-small">
+        Logout</a>
+        </template>
       </div>
     </div>
     <div class="main">
@@ -138,27 +135,6 @@ p {
   color: #222;
   text-transform: uppercase;
   font-weight: 600;
-}
-@media (max-width: 414px) {
-  .menu-right {
-    display: flex;
-    align-content: flex-end;
-
-  }
-  .btn-group-menu {
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    justify-content: flex-end;
-  }
-  .btn-small {
-    max-width: 100px;
-    padding: 0;
-    background: white;
-    margin: 5px;
-    color: #37baf7;
-    font-weight: 900;
-  }
 }
 
 </style>
