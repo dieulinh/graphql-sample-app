1dc2358dbc38e044eab9bda34c7302d64f91e046
Linh Nguyen <linhvn09@gmail.com>
2022-08-24 01:25:25 +0700

    allow more api access

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'iclass-fe.herokuapp.com', 'localhost:3000', 'www.bikiin.com'
+    origins 'iclass-fe.herokuapp.com', 'localhost:3000', 'www.bikiin.com', 'fe-bikiin.herokuapp.com'
     resource '/public/*', headers: :any, methods: :get
 
     # Only allow a request for a specific host
