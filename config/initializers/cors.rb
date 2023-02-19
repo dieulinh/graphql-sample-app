2c6abc30eb06acaffeef3b5b216e128503ef8eb2
Linh Nguyen <linh.nguyen@unifiedpost.com>
2023-02-19 14:24:13 +0700

    add more enpoint to allow call openai

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'iclass-fe.herokuapp.com', 'localhost:3000', 'www.bikiin.com', 'fe-bikiin.herokuapp.com'
+    origins 'iclass-fe.herokuapp.com', 'localhost:3000', 'www.bikiin.com', 'fe-bikiin.herokuapp.com', 'https://chat.openai.com', "https://chat.openai.com/fonts"
     resource '/public/*', headers: :any, methods: :get
 
     # Only allow a request for a specific host
