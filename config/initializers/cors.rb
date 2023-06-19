12b39e22fad02df229668c4e2734b2ba065e3112
Linh Nguyen <linh.nguyen@unifiedpost.com>
2023-06-19 18:40:15 +0700

    update server list

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'iclass-fe.herokuapp.com', 'localhost:3000', 'www.bikiin.com', 'fe-bikiin.herokuapp.com', 'https://chat.openai.com', "https://chat.openai.com/fonts"
+    origins 'iclass-fe.herokuapp.com', 'http://localhost:3000', 'www.bikiin.com', 'fe-bikiin.herokuapp.com', 'imentor-daa90261bfc9.herokuapp.com'
     resource '/public/*', headers: :any, methods: :get
 
     # Only allow a request for a specific host
