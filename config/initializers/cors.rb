96ccbf3a499c0373c001139c6a7616236ea4fbf3
dieulinh <linhvn09@gmail.com>
2024-02-20 11:09:19 -0500

    less strict endpoint

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'iclass-fe.herokuapp.com', 'http://localhost:3000', 'www.bikiin.com', 'i-work.info', ENV['NEW_APP_ENDPOINT']
-    resource '/public/*', headers: :any, methods: :get
+    # resource '/public/*', headers: :any, methods: [:get, :options, :head]
 
     # Only allow a request for a specific host
-    resource '/api/*',
+    resource '*',
         headers: :any,
         methods: [:get,
           :post,
