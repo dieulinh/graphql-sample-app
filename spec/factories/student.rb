2b5d6f6cbf8eb7a63b2d67eb3e903d7be53312d6
dieulinh <linhvn09@gmail.com>
2024-07-16 11:21:19 -0400

    resolve conflict

--- spec/factories/student.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-FactoryBot.define do
-  factory :student do
-    email { Faker::Internet.email }
-    password { 'password' }
-    password_confirmation { 'password' }
-  end
-end
