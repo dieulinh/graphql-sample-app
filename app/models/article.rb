4527b87d5365e925066a2bc663ddf6be39195d00
dieulinh <linhvn09@gmail.com>
2024-12-16 11:42:53 -0500

    only show published articles for blogging

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,14 @@ class Article < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   has_many_attached :photos
+
+  default_scope -> { where(published: true) }
+  # def self.parse(url_str)
+  #   url = URI.parse(url_str)
+  #   return '' if url.host.nil?
+  #
+  #   return
+  #   url.host.repace('www','').split('.')[0]
+  # end
+
 end
